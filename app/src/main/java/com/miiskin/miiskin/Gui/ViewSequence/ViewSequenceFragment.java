65f5b1b02f152773b98a31726e62c09feb273482
Sednev Michail <sednevmxail@gmailc.om>
2015-06-26 11:29:43 +0400

    view sequence buttons animation

--- app/src/main/java/com/miiskin/miiskin/Gui/ViewSequence/ViewSequenceFragment.java
+++ app/src/main/java/com/miiskin/miiskin/Gui/ViewSequence/ViewSequenceFragment.java
@@ -1,8 +1,15 @@
 package com.miiskin.miiskin.Gui.ViewSequence;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +24,13 @@ public class ViewSequenceFragment extends Fragment {
     public static final String EXTRA_SEQUENCE_DATA = "EXTRA_SEQUENCE_DATA";
 
     SequenceData mSequenceData;
+    FloatingActionButton mFloatingActionButton;
+    FloatingActionButton mSendToDoctor;
+    FloatingActionButton mTakePhoto;
+    CoordinatorLayout mCoordinatorLayout;
+    private float mYDoctorInitialPosition;
+    private float mYPhotoInitialPosition;
+    private boolean mButtonsVisible = false;
 
     public static ViewSequenceFragment newInstance(SequenceData sequenceData) {
         ViewSequenceFragment fragment = new ViewSequenceFragment();
@@ -29,9 +43,11 @@ public class ViewSequenceFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
         if (getArguments() != null) {
             mSequenceData = (SequenceData)getArguments().getSerializable(EXTRA_SEQUENCE_DATA);
         }
+
     }
 
     @Override
@@ -39,6 +55,22 @@ public class ViewSequenceFragment extends Fragment {
         super.onResume();
         final ViewSequenceActivity viewSequenceActivity = (ViewSequenceActivity)getActivity();
         viewSequenceActivity.mActionBarToolbar.setTitle(mSequenceData.mBodyPart.toString());
+        mFloatingActionButton.post(new Runnable() {
+            @Override
+            public void run() {
+                mYDoctorInitialPosition = mSendToDoctor.getY();
+                mYPhotoInitialPosition = mTakePhoto.getY();
+            }
+        });
+        if (mButtonsVisible) {
+            mSendToDoctor.setVisibility(View.VISIBLE);
+            mTakePhoto.setVisibility(View.VISIBLE);
+            mFloatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_clear));
+        } else {
+            mSendToDoctor.setVisibility(View.INVISIBLE);
+            mTakePhoto.setVisibility(View.INVISIBLE);
+            mFloatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_add));
+        }
     }
 
     @Override
@@ -52,11 +84,99 @@ public class ViewSequenceFragment extends Fragment {
                 getActivity().onBackPressed();
             }
         });
+        mFloatingActionButton = (FloatingActionButton)view.findViewById(R.id.fab);
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mButtonsVisible) {
+                    showButtons();
+                } else {
+                    hideButtons();
+                }
+
+            }
+        });
+        mSendToDoctor = (FloatingActionButton)view.findViewById(R.id.sendDoctor);
+        mSendToDoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToDoctor();
+            }
+        });
+        mTakePhoto = (FloatingActionButton)view.findViewById(R.id.takePhoto);
+        mTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto();
+            }
+        });
+        mCoordinatorLayout = (CoordinatorLayout)view.findViewById(R.id.coordinatorLayout);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_view_sequence, container, false);
+    }
+
+    private void sendToDoctor() {
+
+    }
+
+    private void takePhoto() {
+
+    }
+
+    private void showButtons() {
+        float centerFabY = mFloatingActionButton.getY() + mFloatingActionButton.getHeight() / 2;
+
+        float yDoctorFrom = centerFabY - mSendToDoctor.getHeight() / 2;
+        float yPhotoFrom = centerFabY - mTakePhoto.getHeight() / 2;
+
+        ValueAnimator doctorTransitionAnimator = ObjectAnimator.ofFloat(mSendToDoctor, "y", yDoctorFrom, mYDoctorInitialPosition);
+        ValueAnimator photoTransitionAnimator = ObjectAnimator.ofFloat(mTakePhoto, "y", yPhotoFrom, mYPhotoInitialPosition);
+
+        AnimatorSet animPhotoAndDoctorAppearing = new AnimatorSet();
+        animPhotoAndDoctorAppearing.playTogether(doctorTransitionAnimator, photoTransitionAnimator);
+        animPhotoAndDoctorAppearing.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                mSendToDoctor.setVisibility(View.VISIBLE);
+                mTakePhoto.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mButtonsVisible = true;
+                mFloatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_clear));
+            }
+        });
+        animPhotoAndDoctorAppearing.setDuration(250);
+        animPhotoAndDoctorAppearing.start();
+    }
+
+    private void hideButtons() {
+        float centerFabY = mFloatingActionButton.getY() + mFloatingActionButton.getHeight() / 2;
+
+        float yDoctorTo = centerFabY - mSendToDoctor.getHeight() / 2;
+        float yPhotoTo = centerFabY - mTakePhoto.getHeight() / 2;
+
+        ValueAnimator doctorTransitionAnimator = ObjectAnimator.ofFloat(mSendToDoctor, "y", mYDoctorInitialPosition, yDoctorTo);
+        ValueAnimator photoTransitionAnimator = ObjectAnimator.ofFloat(mTakePhoto, "y", mYPhotoInitialPosition, yPhotoTo);
+
+        AnimatorSet animPhotoAndDoctorAppearing = new AnimatorSet();
+        animPhotoAndDoctorAppearing.playTogether(doctorTransitionAnimator, photoTransitionAnimator);
+        animPhotoAndDoctorAppearing.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mSendToDoctor.setVisibility(View.INVISIBLE);
+                mTakePhoto.setVisibility(View.INVISIBLE);
+                mButtonsVisible = false;
+                mFloatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_add));
+            }
+        });
+        animPhotoAndDoctorAppearing.setDuration(250);
+        animPhotoAndDoctorAppearing.start();
     }
 }

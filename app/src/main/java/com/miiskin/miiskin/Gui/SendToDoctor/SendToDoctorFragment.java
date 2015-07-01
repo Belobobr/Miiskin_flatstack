cbfd27fa6dfd63232ac4a49691150b042f4f60a3
Sednev Michail <sednevmxail@gmailc.om>
2015-07-01 21:06:41 +0400

    check next photo date

--- app/src/main/java/com/miiskin/miiskin/Gui/SendToDoctor/SendToDoctorFragment.java
+++ app/src/main/java/com/miiskin/miiskin/Gui/SendToDoctor/SendToDoctorFragment.java
@@ -1,18 +1,22 @@
 package com.miiskin.miiskin.Gui.SendToDoctor;
 
+import android.app.DialogFragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
 
+import com.miiskin.miiskin.Gui.Camera.FteCameraTipsFragment;
 import com.miiskin.miiskin.R;
 
 /**
  * Created by Newshka on 27.06.2015.
  */
-public class SendToDoctorFragment extends Fragment{
+public class SendToDoctorFragment extends Fragment implements UnderDevelopmentDialog.UnderDevelopmentDialogListener{
 
     public static SendToDoctorFragment newInstance() {
         SendToDoctorFragment fragment = new SendToDoctorFragment();
@@ -20,6 +24,8 @@ public class SendToDoctorFragment extends Fragment{
         fragment.setArguments(arguments);
         return fragment;
     }
+
+    private CheckBox mLicenseAgreementCheckBox;
 
     @Nullable
     @Override
@@ -30,5 +36,46 @@ public class SendToDoctorFragment extends Fragment{
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        final View aboutOurDoctor = view.findViewById(R.id.about_our_doctor);
+        final View aboutOurDoctorDescription = view.findViewById(R.id.about_our_doctor_description);
+        aboutOurDoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (aboutOurDoctorDescription.getVisibility() == View.VISIBLE) {
+                    aboutOurDoctorDescription.setVisibility(View.GONE);
+                } else {
+                    aboutOurDoctorDescription.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        Button sendToDoctorButton = (Button)view.findViewById(R.id.send_to_doctor_button);
+        sendToDoctorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkEmail() && checkAgreement()) {
+                    DialogFragment dialog = new UnderDevelopmentDialog();
+                    dialog.show(getActivity().getFragmentManager(), UnderDevelopmentDialog.TAG);
+                }
+            }
+        });
+        mLicenseAgreementCheckBox = (CheckBox)view.findViewById(R.id.licence_agreement_check_box);
+    }
+
+    private boolean checkEmail() {
+        return true;
+    }
+
+    private boolean checkAgreement() {
+        return  mLicenseAgreementCheckBox.isChecked();
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+
+    }
+
+    @Override
+    public void onDialogBackButtonPressed() {
+
     }
 }
---
+++ app/src/main/java/com/miiskin/miiskin/Gui/SendToDoctor/UnderDevelopmentDialog.java
@@ -0,0 +1,75 @@
+package com.miiskin.miiskin.Gui.SendToDoctor;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+
+import com.miiskin.miiskin.R;
+
+/**
+ * Created by Newshka on 01.07.2015.
+ */
+public class UnderDevelopmentDialog extends DialogFragment {
+
+    public final static String TAG = "FTE_CAMERA_TIP_DIALOG";
+
+    public interface UnderDevelopmentDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogBackButtonPressed();
+    }
+
+    UnderDevelopmentDialogListener mListener;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (UnderDevelopmentDialogListener) activity;
+
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getDialog().setOnKeyListener(new DialogInterface.OnKeyListener()
+        {
+            @Override
+            public boolean onKey(android.content.DialogInterface dialog, int keyCode,android.view.KeyEvent event) {
+
+                if ((keyCode ==  android.view.KeyEvent.KEYCODE_BACK))
+                {
+                    mListener.onDialogBackButtonPressed();
+                }
+                return false;
+
+            }
+        });
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.DisclaimerAlertDialog);
+        builder.setTitle(R.string.under_development);
+        builder.setMessage(R.string.under_development_description)
+                .setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        mListener.onDialogPositiveClick(UnderDevelopmentDialog.this);
+                    }
+                })
+                .setNegativeButton(R.string.send_feedback, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        return builder.create();
+    }
+
+}
